cb6a1b51a77b8c8056ff8c4b1569ed053e3a4cb5
Pedro Papini <ppapinia@gmail.com>
2023-03-22 15:35:59 -0300

    Resolve requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui!"""
-    raise NotImplementedError
+from typing import List, Tuple
+
+
+def study_schedule(
+    permanence_period: List[Tuple[int, int]], target_time: int
+) -> int:
+    if not isinstance(target_time, int):
+        return None
+
+    counter = 0
+
+    for student_permanence_period in permanence_period:
+        (entry_time, exit_time) = student_permanence_period
+
+        if not isinstance(entry_time, int) or not isinstance(exit_time, int):
+            return None
+
+        if entry_time <= target_time <= exit_time:
+            counter += 1
+
+    return counter
