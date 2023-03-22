524165f6b66915b9da21b8cc9c2323e17bbeeff2
Pedro Papini <ppapinia@gmail.com>
2023-03-22 17:49:25 -0300

    Resolve requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums: list[int]) -> int:
+    if not nums:
+        return False
+
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
